a8e7a6c9c422fd6c2a84a86f67180c49e4dce73f
Unknown <83038940+kakUnknownnnnn@users.noreply.github.com>
2021-07-17 03:52:58 +0300

    Create UnKnown.js

---
+++ UnKnown.js
@@ -0,0 +1,68 @@
+const discord = require("discord.js")
+const client =new discord.Client()
+const talkedRecently = new Set();    
+
+
+
+client.on("ready", () => {
+  console.log("UnKnown")
+  client.user.setStatus("ONLINE");
+
+  client.user.setActivity(`Project By UnKnown`, { type: "PLAYING" });
+
+  client.guilds.cache.forEach(g => {
+
+    if (g.member(client.user).hasPermission("ADMINISTRATOR")) {
+
+      g.fetchInvites().then(guildInvites => {});
+
+    }
+
+  });
+
+});
+
+
+
+client.on("message", vex => { 
+if(vex.content.includes("discord.gg")){ 
+    
+    if (talkedRecently.has(vex.author.id)) {
+      vex.author.send("Plase wait for 3 Hours and again send it");
+    } else {
+
+if(vex.channel.type === "dm"){ 
+if(vex.author.id===client.user.id) return; 
+vex.author.send("")
+let s2 = vex .content = vex.content.replace(/@(everyone)/, "🥰").replace(/@(here)/, "🥰");
+let hama1 = client.channels.cache.get("")//id bashe reklam
+hama1.send(`
+ناردرا لە لایەن
+<@${vex.author.id}> ` +`
+سێرڤەرەکە
+${s2}
+          `)
+  }
+
+}}
+  talkedRecently.add(vex.author.id);
+        setTimeout(() => {
+          talkedRecently.delete(vex.author.id);
+        }, 60000);
+  
+  
+})
+
+const channelid = ""//id channele vc
+client.on("ready", () => {
+    const channel = client.channels.cache.get(channelid);
+    if (!channel) return console.error("The channel does not exist!");
+    channel.join().then(connection => {
+        console.log("Successfully connected.");
+    }).catch(e => {
+        console.error(e);
+    });
+});
+
+
+client.login("")
